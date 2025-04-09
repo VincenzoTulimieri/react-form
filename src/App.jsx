62184c025aae5ead8db5f1f25e47da4f55bb64f6
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-04-09 15:43:22 +0200

    feat: importato file data

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@
 import { useState } from 'react'
 import Header from './components/Header/Header'
 import Main from './components/Main/Main'
+import mangaList from './data/data'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Header />
-    <Main />
+      <Header />
+      <Main mangaList={mangaList} />
     </>
   )
 }
