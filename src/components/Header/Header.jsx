a7276ea333cd514c460d487e2c289c7f2cfb60fd
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-04-09 16:24:23 +0200

    feat: creata lista

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,9 @@
 export default function Header(){
     return(
         <header>
-
+            <div className="container">
+                <h1 className="header-title">la tua mangalist</h1>
+            </div>
         </header>
     )
 }
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,18 @@
 
-export default function Main(){
+export default function Main(props){
+    const {mangaList} = props
+    console.log(mangaList)
     return(
         <main>
-
+            <div className="container">
+                <ul className="manga-list">
+                    {mangaList.map(manga =>(
+                        <li key={manga.id} >
+                            <h2>{manga.title}</h2>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </main>
     )
 }
