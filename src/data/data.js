137d7f25629e185e1b151b2d30da68439c01f623
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-04-09 15:43:03 +0200

    feat: creati file dati in cartella data

---
+++ src/data/data.js
@@ -0,0 +1,34 @@
+const mangaList = [
+    {
+      id: 1,
+      titolo: "One Piece",
+      descrizione: "Le avventure epiche di Monkey D. Luffy alla ricerca del tesoro leggendario One Piece per diventare il Re dei Pirati.",
+      immagine: "https://example.com/images/one-piece.jpg"
+    },
+    {
+      id: 2,
+      titolo: "Naruto",
+      descrizione: "Naruto Uzumaki, un giovane ninja con il sogno di diventare il leader del suo villaggio, affronta sfide e scopre il suo destino.",
+      immagine: "https://example.com/images/naruto.jpg"
+    },
+    {
+      id: 3,
+      titolo: "Bleach",
+      descrizione: "Ichigo Kurosaki diventa uno Shinigami per proteggere i vivi dagli spiriti maligni e scoprire il vero significato del suo potere.",
+      immagine: "https://example.com/images/bleach.jpg"
+    },
+    {
+      id: 4,
+      titolo: "Black Clover",
+      descrizione: "Asta, un ragazzo nato senza magia, lotta per diventare il Mago Imperatore in un mondo dove la magia è tutto.",
+      immagine: "https://example.com/images/black-clover.jpg"
+    },
+    {
+      id: 5,
+      titolo: "Demon Slayer",
+      descrizione: "Tanjiro Kamado intraprende un viaggio per salvare sua sorella trasformata in demone e vendicarsi per la morte della sua famiglia.",
+      immagine: "https://example.com/images/demon-slayer.jpg"
+    }
+  ];
+  
+  export default mangaList;
