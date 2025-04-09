cd4e8638ff7d0813d53161ef1bfbf0e27861a014
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-04-09 16:57:51 +0200

    feat :creato il form

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,19 @@
+// importazione file
+import { useState } from 'react'
 
 export default function Main(props){
     const {mangaList} = props
     console.log(mangaList)
+    const{manga, setManga}= useState('ciao')
+
+    
+    console.log(useState())
+
+    function sendForm (event){
+        event.preventDefault();
+        console.log(manga)
+    }
+
     return(
         <main>
             <div className="container">
@@ -12,6 +24,10 @@ export default function Main(props){
                         </li>
                     ))}
                 </ul>
+                <form action="" className="form-container" onSubmit={sendForm}>
+                    <input type="text" value={manga} />
+                    <button>invia il manga da leggere</button>
+                </form>
             </div>
         </main>
     )
